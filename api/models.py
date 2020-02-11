0c5f051dea2a35b5ceff336bfa360834de87ca01
Rahul D. Ghosal <rdghosal@gmail.com>
2020-02-11 09:30:58 -0500

    updated models and fixed user_service errs

--- api/models.py
+++ api/models.py
@@ -6,9 +6,9 @@ class Users(db.Model):
     """Stores user info"""
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    registration_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     username = db.Column(db.String(15), nullable=False, unique=True)
-    password = db.Column(db.String(10), nullable=False) 
-    # hashed = db.Column(db.String(255), nullable=False)
+    hashed = db.Column(db.String(255), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     country = db.Column(db.String(255), nullable=False)
     queries = db.relationship("UserQueries", backref="user", lazy=True)
--- api/user_service.py
+++ api/user_service.py
@@ -4,8 +4,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 def check_form_data(form, db, users_table):
     """Takes data used for registration
     and evaluates whether info is original"""
-    if form.password != form.confirmation or \
-        db.session.query(users_table).filter(users_table.username == form.username).count() > 0:
+    if form["password"] != form["confirmation"] or \
+        db.session.query(users_table).filter(users_table.username == form["username"]).count() > 0:
         return False
     return True
 
@@ -13,10 +13,10 @@ def add_user(form, db, users_table):
     """Adds user to database"""
     # Add new user data
     db.session.add(users_table(
-        username=form.username, 
-        hashed=generate_password_hash(form.password),
-        age=form.age,
-        country=form.country
+        username=form["username"], 
+        hashed=generate_password_hash(form["password"]),
+        age=form["age"],
+        country=form["country"]
     ))
 
     # Commit change to db
@@ -25,7 +25,7 @@ def add_user(form, db, users_table):
 
 def get_user_id(form, db, users_table):
     """Checks login credentials against database"""
-    result = db.session.query(users_table).filter(users_table.username == form.username).first()
-    if not result or not check_password_hash(result.hashed, form.password):
+    result = db.session.query(users_table).filter(users_table.username == form["username"]).first()
+    if not result or not check_password_hash(result.hashed, form["password"]):
         return None
-    return result.user_id
+    return result.id
